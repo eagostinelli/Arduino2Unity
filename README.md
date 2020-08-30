780abc317bf472eda6b2c5c039585d5627b1107e
eagostinelli <erika.agostinelli13@gmail.com>
2020-08-30 16:45:56 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Arduino2Unity
-How to send input from Arduino to Unity 3D - playground
+
+
+##How to send input from Arduino to Unity 3D - playground
+
+<img src="./img/test_unity.gif" width="350" />
+
+
+In this repository I will collect the script for 
+1. Arudino: the user could easily use this code to have a simple board with two LEDs that light up if a button is pressed (right-left). 
+2. Unity3D: the user will need to attach this C# script to the object of interest and will be able to move the object in Unity by usgin the button on Arduino. 
+
+Blog with instructions: coming soon
+
+
+Author: <a href="https://www.linkedin.com/in/erikaagostinelli/"> Erika Agostinelli </a>
---
+++ Unity3D_script/Move.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class Move : MonoBehaviour
+{
+    SerialPort sp = new SerialPort("/dev/tty.usbmodem144201", 9600);
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        sp.Open();
+        sp.ReadTimeout = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (sp.IsOpen){
+            try{
+                if(sp.ReadByte()==1){
+                    print(sp.ReadByte());
+                    transform.Translate(Vector3.left * Time.deltaTime * 5);
+                }
+                if(sp.ReadByte()==2){
+                    print(sp.ReadByte());
+                    transform.Translate(Vector3.right * Time.deltaTime * 5);
+                }
+            }
+            catch (System.Exception){
+               
+            }
+
+        }
+    }
+}
